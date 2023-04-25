6093e51be202b464efc082ec9113f759807bf2a3
Tishka <yuriitish@mail.ru>
2023-04-25 15:28:47 +0400

    добавил модалку

---
+++ js/modal.js
@@ -0,0 +1,6 @@
+function openModal() {
+    document.querySelector(".modal-icon").classList.remove("removed");
+}
+function closeModal() {
+    document.querySelector(".modal-icon").classList.add("removed");
+}
